57cdb2a2cc633e1525db2bbfe6ca65a4adce7725
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2023-01-22 03:44:43 +0100

    code review

--- test-ansible.sh
+++ test-ansible.sh
@@ -6,6 +6,8 @@ set +a
 
 #ansible-galaxy collection install cyberark.conjur
 
+rm -rf "$CONJUR_CERT_FILE"
+
 openssl s_client -connect "$CONJUR_MASTER_HOSTNAME":"$CONJUR_MASTER_PORT" \
   -showcerts </dev/null 2> /dev/null | \
   awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/ {print $0}' \
