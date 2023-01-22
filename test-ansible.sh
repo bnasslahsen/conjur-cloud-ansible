30e2f0f553d541d1994357e68bd29cf387a856c8
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2023-01-22 03:30:17 +0100

    code review

--- test-ansible.sh
+++ test-ansible.sh
@@ -11,6 +11,6 @@ openssl s_client -connect "$CONJUR_MASTER_HOSTNAME":"$CONJUR_MASTER_PORT" \
   awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/ {print $0}' \
   > "$CONJUR_CERT_FILE"
 
-cat "Amazon Root CA 1.cer" >> "$CONJUR_SSL_CERTIFICATE"
+cat conjur-cloud-ca >> "$CONJUR_SSL_CERTIFICATE"
 
 ansible-playbook -i inventory playbook.yml
