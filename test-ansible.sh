c894cbd098536c6587c0b433b7aacb2fc9d48852
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2023-01-22 04:40:42 +0100

    code review

--- test-ansible.sh
+++ test-ansible.sh
@@ -8,11 +8,11 @@ set +a
 
 rm -rf "$CONJUR_CERT_FILE"
 
+cat conjur-cloud-ca > "$CONJUR_CERT_FILE"
+
 openssl s_client -connect "$CONJUR_MASTER_HOSTNAME":"$CONJUR_MASTER_PORT" \
   -showcerts </dev/null 2> /dev/null | \
   awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/ {print $0}' \
-  > "$CONJUR_CERT_FILE"
-
-cat conjur-cloud-ca >> "$CONJUR_CERT_FILE"
+  >> "$CONJUR_CERT_FILE"
 
 ansible-playbook -i inventory playbook.yml
